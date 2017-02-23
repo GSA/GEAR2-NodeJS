d6112398ad68ce617dbd42ca0fe4e1a01676bfcc
mdodson_gsaea <matthew.dodson.dc@gmail.com>
2017-02-23 12:13:50 -0500

    GE-450 mods to use new SQL function-based requests from MSSQL

--- routes/api/application.js
+++ routes/api/application.js
@@ -8,12 +8,9 @@ console.log('application routes loaded');
 var router = express.Router();
 
 router.route('/')
-    .post(appCtrl.create)
     .get(appCtrl.findAll);
 
 router.route('/:id')
-    .get(appCtrl.findOne)
-    .put(appCtrl.update)
-    .delete(appCtrl.remove);
+    .get(appCtrl.findOne);
 
 module.exports = router;
