fbe3d026b866c5998ae633aa0cf9fa69337e4492
aliang-gsa <amy.liang@gsa.gov>
2017-04-26 09:44:14 -0400

    feature clickable

--- models/managerpage.js
+++ models/managerpage.js
@@ -14,35 +14,66 @@ class MNGtandardsModel extends Model {
       {
         name: 'NoFISMA',
         type: 'int',
+         mapping(d)
+        {
+          let s = '';          
+            s = "<a href='#!applications/find/Name=&SSO=&OwnerShort=&BusinessPOC=&TechnicalPOC=&FISMASystem=none&System=&Status=' target = '_blank'>" + d.NoFISMA + "</a>"
+          return s;
+        },
 
       },
       {
         name: 'NoInvest',
         type: 'int',
-
+         mapping(d)
+        {
+          let s = '';          
+            s = "<a href='#!applications/find/Name=&SSO=&OwnerShort=&BusinessPOC=&TechnicalPOC=&FISMASystem=&System=&Investment=none&Status=' target = '_blank'>" + d.NoInvest + "</a>"
+          return s;
+        },
       },
 	  
       {
         name: 'NoPOCs',
         type: 'int',
-
+         mapping(d)
+        {
+          let s = '';          
+            s = "<a href='#!applications/find/Name=&SSO=&OwnerShort=&BusinessPOC=none&TechnicalPOC=none&System=&Status=' target = '_blank'>" + d.NoPOCs + "</a>"
+          return s;
+        },
       },
 	  
       {
         name: 'NoOwnOrg',
         type: 'int',
-
+         mapping(d)
+        {
+          let s = '';          
+            s = "<a href='#!applications/find/Name=&SSO=&OwnerShort=none&BusinessPOC=&TechnicalPOC=&System=&Status=' target = '_blank'>" + d.NoOwnOrg + "</a>"
+          return s;
+        },
       },
 	  
       {
         name: 'NoAppPlat',
         type: 'int',
-
+         mapping(d)
+        {
+          let s = '';          
+            s = "<a href='#!applications/find/Name=&SSO=&OwnerShort=&BusinessPOC=&TechnicalPOC=&TechnologyPlatform=none&System=&Status=' target = '_blank'>" + d.NoAppPlat + "</a>"
+          return s;
+        },
       },
       {
         name: 'NoTech',
         type: 'int',
-
+         mapping(d)
+        {
+          let s = '';          
+            s = "<a href='#!applications/find/Name=&SSO=&OwnerShort=&BusinessPOC=&TechnicalPOC=&Technologies=none&System=&Status=' target = '_blank'>" + d.NoTech + "</a>"
+          return s;
+        },
       },
 
       /* {
