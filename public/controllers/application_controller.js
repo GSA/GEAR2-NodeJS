a3569967023e1fc5c882f1b6dc3a7fa596789b88
aliang-gsa <amy.liang@gsa.gov>
2017-05-10 10:30:15 -0400

    cleanup

--- public/controllers/application_controller.js
+++ public/controllers/application_controller.js
@@ -616,26 +616,7 @@ function ($route, $scope, $http, $routeParams, $filter, $location, $sce, $window
         $scope.sysDescription = val.Description;
         $scope.sysURL = val.URL;
       });
-      // Use the System 'query' method to send an appropriate GET request
-      //LEGACY
-	  //var apps = ApplicationsSrc.query();
-      /* apps.$promise.then(function (populateApps) {
-        var appgroup = [];
-        $.each(apps, function (key, val) {
-          if (val.ParentSystem == $scope.sysName){
-            appgroup.push({
-				"Name" : val.Name,
-				"Description" : val.Description,
-				"SSO" : val.SSO_Display_Name,
-				"Status" : val.Status,
-				"Id" : val.Id,
-				"Alias": val.Alias,
-				"BusinessPOC": val.BusinessPOC,
-				"TechnicalPOC": val.TechnicalPOC,
-				"Owner":val.Owner
-				});
-          }
-        }); */
+
 		var sysapp = SysAppSrc.query({ id: $routeParams.id });
         sysapp.$promise.then(function (populateData) {
 
@@ -854,277 +835,6 @@ function ($route, $scope, $http, $routeParams, $filter, $location, $sce, $window
     }
   });
 
-  /* $scope.createInterfaceSSOChart1 = function (appId, orgName) {
-    // TODO: there are better ways filtering Interfaces. Let's choose one that isn't dependent on args like this. -mld
-    var interfaces = null,
-        CONTAINER_ID = 'interfacessochart',
-        SVG_ID = 'interfacesvg';
-
-    if (document.getElementById(SVG_ID)) {
-      return false;
-    }
-
-    if (appId && !orgName) {
-      interfaces = AppInterfacesSrc.query({ id: appId });
-    } else if (!appId && orgName) {
-      interfaces = InterfacesSrc.query({ owner: orgName });
-    } else {
-      interfaces = InterfacesSrc.query();
-    }
-    interfaces.$promise.then(function () {
-      // Considering that each Interface has 2 Applications: Name1 & Name2, AppID1 & AppID2, etc...
-      // . Gather all "1"s into a "source nodes" collection
-      var sourceNodes = _.map(interfaces, function (el) {
-        return {
-          name: el.Name1,
-          group: el.Owner1,
-        }
-      });
-      // . Gather all "2"s into a "target nodes" collection
-      var targetNodes = _.map(interfaces, function (el) {
-        return {
-          name: el.Name2,
-          group: el.Owner2,
-        }
-      });
-      // . Generate a list of the applications used across sourceNodes & targetNodes
-      var nodes = sourceNodes.concat(targetNodes);
-      nodes = _.sortBy(nodes, 'name');
-      nodes = _.uniq(nodes, true, function (node) {
-        return node.name + '--' + node.group;
-      });
-
-      // . translates our Interfaces API output into link objects where source is "1" and target is "2"
-      // and their index values are retrieved by matching the Application.Name to a name in the nodes[]
-      // array we created.
-      var links = _.map(interfaces, function (el) {
-        return {
-          source: getIndex(el.Name1),
-          target: getIndex(el.Name2),
-        }
-      });
-
-      // . A utility. Because the visualization pulls data from the nodes[] collection using index
-      function getIndex(appName) {
-        var ind = -1;
-
-        _.find(nodes, function (el, i) {
-          if (el.name == appName) {
-            ind = i;
-          }
-        });
-
-        return ind;
-      }
-
-      var finallist = {"nodes" : nodes, "links" : links};
-
-      //Constants for the SVG
-      var width = $('#' + CONTAINER_ID).parents('.panel-body').width(),
-      height = 500;
-
-      // Because sometimes in IE, $.width() returns 0
-      if (!width) {
-        width = 930; // best fit @1280px screen width in IE11
-      }
-
-      //Set up the colour scale
-      var color = d3.scale.category20();
-
-      //Set up the force layout
-      var force = d3.layout.force()
-      .charge(-120)
-      .linkDistance(30)
-      .size([width, height]);
-
-
-      var node_drag = d3.behavior.drag()
-      .on("dragstart", dragstart)
-      .on("drag", dragmove)
-      .on("dragend", dragend);
-      function dragstart(d, i) {
-        force.stop() // stops the force auto positioning before you start dragging
-      }
-      function dragmove(d, i) {
-        d.px += d3.event.dx;
-        d.py += d3.event.dy;
-        d.x += d3.event.dx;
-        d.y += d3.event.dy;
-      }
-      function dragend(d, i) {
-        d.fixed = true; // of course set the node to fixed so the force doesn't include the node in its auto positioning stuff
-        force.resume();
-      }
-      function releasenode(d) {
-        d.fixed = false; // of course set the node to fixed so the force doesn't include the node in its auto positioning stuff
-        //force.resume();
-      }
-
-
-
-      //Append a SVG to the body of the html page. Assign this SVG as an object to svg
-      var svg = d3.select('#' + CONTAINER_ID).append("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .attr("id", SVG_ID);
-      //Read the data from the finallist element
-      var graph = finallist;
-
-      var padding = 10, // separation between circles
-      radius=8;
-      function collide(alpha) {
-        var quadtree = d3.geom.quadtree(graph.nodes);
-        return function(d) {
-          var rb = 2*radius + padding,
-          nx1 = d.x - rb,
-          nx2 = d.x + rb,
-          ny1 = d.y - rb,
-          ny2 = d.y + rb;
-          quadtree.visit(function(quad, x1, y1, x2, y2) {
-            if (quad.point && (quad.point !== d)) {
-              var x = d.x - quad.point.x,
-              y = d.y - quad.point.y,
-              l = Math.sqrt(x * x + y * y);
-              if (l < rb) {
-                l = (l - rb) / l * alpha;
-                d.x -= x *= l;
-                d.y -= y *= l;
-                quad.point.x += x;
-                quad.point.y += y;
-              }
-            }
-            return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
-          });
-        };
-      }
-
-      force
-      .nodes(graph.nodes)
-      .links(graph.links)
-      .start();
-
-      var link = svg.selectAll(".link")
-      .data(graph.links)
-      .enter().append("line")
-      .attr("class", "link")
-      .style("stroke-width", function(d) { return Math.sqrt(d.value); });
-
-      var node = svg.selectAll(".node")
-      .data(graph.nodes)
-      .enter().append("g")
-      .attr("class", "node")
-      .on('dblclick', releasenode)
-      .call(node_drag) //Added
-      .on('click', connectedNodes); //Added code
-      node.append("circle")
-      .attr("r", 8)
-      .style("fill", function (d) {
-        return color(d.group);
-      })
-
-      node.append("text")
-      .attr("class", "labeltext")
-      .attr("dx", 10)
-      .attr("dy", ".35em")
-      .text(function(d) { return d.name });
-
-
-      //Insert Legend
-
-      var legend = svg.selectAll(".legend")
-      .data(color.domain())
-      .enter().append("g")
-      .attr("class", "legend")
-      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
-
-      legend.append("rect")
-      .attr("x", width - 18)
-      .attr("width", 18)
-      .attr("height", 18)
-      .style("fill", color);
-
-      legend.append("text")
-      .attr("x", width - 24)
-      .attr("y", 9)
-      .attr("dy", ".35em")
-      .style("text-anchor", "end")
-      .text(function(d) { return d; });
-
-
-
-
-
-      force.on("tick", function() {
-        link.attr("x1", function (d) {
-          return d.source.x;
-        })
-        .attr("y1", function (d) {
-          return d.source.y;
-        })
-        .attr("x2", function (d) {
-          return d.target.x;
-        })
-        .attr("y2", function (d) {
-          return d.target.y;
-        });
-        d3.selectAll("circle").attr("cx", function (d) {
-          return d.x;
-        })
-        .attr("cy", function (d) {
-          return d.y;
-        });
-        d3.selectAll(".labeltext").attr("x", function (d) {
-          return d.x;
-        })
-        .attr("y", function (d) {
-          return d.y;
-        });
-        node.each(collide(0.5)); //Added
-      });
-
-
-      //Toggle stores whether the highlighting is on
-      var toggle = 0;
-
-      //Create an array logging what is connected to what
-      var linkedByIndex = {};
-      for (var i = 0; i < graph.nodes.length; i++) {
-        linkedByIndex[i + "," + i] = 1;
-      };
-      graph.links.forEach(function (d) {
-        linkedByIndex[d.source.index + "," + d.target.index] = 1;
-      });
-
-      //This function looks up whether a pair are neighbours
-      function neighboring(a, b) {
-        return linkedByIndex[a.index + "," + b.index];
-      }
-
-      function connectedNodes() {
-        if (toggle == 0) {
-          //Reduce the opacity of all but the neighbouring nodes
-          var d = d3.select(this).node().__data__;
-          node.style("opacity", function (o) {
-            return neighboring(d, o) | neighboring(o, d) ? 1 : 0.1;
-          });
-
-          link.style("opacity", function (o) {
-            return d.index==o.source.index | d.index==o.target.index ? 1 : 0.1;
-          });
-
-          //Reduce the op
-          toggle = 1;
-        } else {
-          //Put them back to opacity=1
-          node.style("opacity", 1);
-          link.style("opacity", 1);
-          toggle = 0;
-        }
-      }
-    });
-<<<<<<< HEAD
-   }*/
-  
   
   
   $scope.createInterfaceSSOChart = function (appId, orgName) {
@@ -1175,7 +885,7 @@ function ($route, $scope, $http, $routeParams, $filter, $location, $sce, $window
 		var mpr = chordMpr(data);
 
 			mpr
-			  .addValuesToMap("NameShort1")//,['NameShort1','Owner1','OwnerShort1','SSO1','SSOShort1'])
+			  .addValuesToMap("NameShort1")
 			  
 			  .setFilter(function (row, a, b) {
             return (row.NameShort1 === a.name && row.NameShort2 === b.name);
@@ -1187,12 +897,12 @@ function ($route, $scope, $http, $routeParams, $filter, $location, $sce, $window
 			drawChords(mpr.getMatrix(), mpr.getMap());
 	//	}) 
 	function drawChords (matrix, mmap) {
-		var r1 = h / 2, r0 = 0.6 * r1;//r1 - 80; //w = 880, h = 700, 
-	   // var w = 980, h = 800, r1 = h / 2, r0 = r1 - 100;
+		var r1 = h / 2, r0 = 0.6 * r1;
+
 		var color = d3.scale.category20b();
 		var fill = d3.scale.ordinal()
                  .range(['#6b6ecf','#b5cf6b','#e7ba52','#d6616b','#de9ed6','#393b79','#637939',	'#8c6d31','#843c39','#7b4173','#ce6dbd','#9c9ede','#cedb9c','#e7cb94','#e7969c','#5254a3','#8ca252','#bd9e39','#ad494a','#a55194',])
-            // .range(['#c7b570','#c6cdc7','#335c64','#768935','#507282','#5c4a56','#aa7455','#574109','#837722','#73342d','#0a5564','#9c8f57','#7895a4','#4a5456','#b0a690','#0a3542',]);
+
       var chord = d3.layout.chord()
                     .sortSubgroups(d3.descending)
                     .sortChords(d3.descending);  
@@ -1281,8 +991,6 @@ function ($route, $scope, $http, $routeParams, $filter, $location, $sce, $window
         .style("font-weight", "bold")
 			  .style("text-anchor", "end")
 			  .text(function(d) { return d; });  
-			  // if(mmapsize <=2){
-			  // legend.attr("transform", function(d, i) { return "translate(" + w/6 + "," + i * 20 + ") rotate(-57)"; });}
 					  
 			  
         var chordPaths = svg.selectAll("path.chord")
@@ -1292,10 +1000,6 @@ function ($route, $scope, $http, $routeParams, $filter, $location, $sce, $window
                 .style("stroke-opacity", .4) // set the stroke opacity
                 .style("stroke", "#3182bd")      // set the line colour, #3182bd seems GSA Blue
                 .style("fill", "#c6dbef")      // set the fill colour
-                // .style("stroke", function(d) { return d3.rgb(fill(+rdr(d).sdata.SSO1)).darker(); })//+rdr(d).sdata.SSO1)
-                // .style("fill", function(d) { return fill(+rdr(d).sdata.SSO1); })//+rdr(d).sdata.SSO1)
-                // .style("stroke", function(d) { return d3.rgb(fill(d.source.index)).darker(); })
-                // .style("fill", function(d) { return fill(d.source.index); })
                 .attr("d", d3.svg.chord().radius(r0))
                 .on("mouseover", function (d) {
                   d3.select("#tooltip1")
@@ -1313,21 +1017,13 @@ function ($route, $scope, $http, $routeParams, $filter, $location, $sce, $window
             var p = d3.format(".2%"), q = d3.format(",.1r")
             return "Interface Data:<br/>"
                +"Coming soon..."
-             // + p(d.svalue/d.stotal) + " (" + q(d.svalue) + ") of "
-              //+ d.sname + " connects to " + d.tname
-              //+ (d.sname === d.tname ? "": ("<br/>while...<br/>"
-             //+ p(d.tvalue/d.ttotal) + " (" + q(d.tvalue) + ") of "
-              //+ d.tname + " prefer " + d.sname))
           }
 
         function groupTip (d) {
             var p = d3.format(".1%"), q = d3.format(",.1r")
             return "Application Information:<br/>"
                     + "Name: "+ d.gnamelong + " <br/>" 
-                    //+ " SSO : " d.gdata.SSO1 + " <br/>"
                     + "Owner: "+ d.gowner + " <br/>"
-                    // + d.gname + " Connects to : " + q(d.gvalue) + " other Applications on this model<br/>"
-                    //+ p(d.gvalue/d.mtotal) + " of Matrix Total (" + q(d.mtotal) + ")"
           }
 
         function mouseclick(d) {
