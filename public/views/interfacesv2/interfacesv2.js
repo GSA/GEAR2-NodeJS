26fe1cdf3214a5fb7edef7529b4ed7d93820c1ac
aliang-gsa <amy.liang@gsa.gov>
2017-06-07 17:24:07 -0400

    make sankey clickable

--- public/views/interfacesv2/interfacesv2.js
+++ public/views/interfacesv2/interfacesv2.js
@@ -8,9 +8,11 @@ angular.module('interfacesv2', ['ngRoute'])
     templateUrl: 'views/interfacesv2/interfacesv2.html',
     controller: 'interfacesv2Ctrl'
   });
+
 }])
 
-.factory('interfacesv2Src', ['$resource', 'WcfConfig', function ($resource, WcfConfig) { 
+.factory('interfacesv2Src', ['$resource', 'WcfConfig', '$location', function ($resource, WcfConfig, $location) {
+			
   return $resource('/api/v0/interfacesv2');}])//managerpage/:id
 
 .controller('interfacesv2Ctrl', ['$scope', 'interfacesv2Src', function ($scope, interfacesv2Src) {
@@ -21,7 +23,7 @@ angular.module('interfacesv2', ['ngRoute'])
 		interv2.$promise.then(function (populateData) {
 		$.each(interv2,function(key,val){
 			data.push({
-				"sourceid":val.scrAppID,
+				"sourceid":val.srcAppID,
 				"targetid":val.destAppID,
 				"source":val.srcApp,
 				"target":val.destApp,
@@ -56,8 +58,11 @@ angular.module('interfacesv2', ['ngRoute'])
                  .range(['#6b6ecf','#b5cf6b','#e7ba52','#d6616b','#de9ed6','#393b79','#637939',	'#8c6d31','#843c39','#7b4173','#ce6dbd','#9c9ede','#cedb9c','#e7cb94','#e7969c','#5254a3','#8ca252','#bd9e39','#ad494a','#a55194',]);
 
 		// append the svg canvas to the page
-		var svg = d3.select('#' + CONTAINER_ID).append("svg")
-     .attr( "preserveAspectRatio", "xMinYMid meet" )
+		var svg = d3.select('#' + CONTAINER_ID)
+			  // .on("touchstart", nozoom)
+              // .on("touchmove", nozoom)
+			.append("svg")
+			.attr( "preserveAspectRatio", "xMinYMid meet" )
 			.attr("width", w)//width + margin.left + margin.right)
 			.attr("height", h)//height + margin.top + margin.bottom)
 			.attr("id", SVG_ID)
@@ -76,44 +81,47 @@ angular.module('interfacesv2', ['ngRoute'])
 		var path = sankey.link();
 		
 
-		// load the data (using the timelyportfolio csv method)
-		// d3.csv("sankey.csv", function(error, data) {
-
+		// load the data
 		  //set up graph in same style as original example but empty
 		 var  graph = {"nodes" : [], "links" : []};
+		 var nodes_k = [];
 
 			data.forEach(function (d) {
-			  graph.nodes.push({ "name": d.source });
-			  // graph.nodes.push({ "name": d.source,
-								 // "id": d.sourceid});
-			  graph.nodes.push({ "name": d.target });
-			  // graph.nodes.push({ "name": d.target,
-								 // "id": d.targetid });
+			  // graph.nodes.push({ "name": d.source });
+			  graph.nodes.push({ "name": d.source,
+								 "id": d.sourceid});
+			  // graph.nodes.push({ "name": d.target });
+			  graph.nodes.push({ "name": d.target,
+								 "id": d.targetid });
 			  graph.links.push({ "source": d.source,
 								 "target": d.target,
-								 // "sourceid": d.sourceid,
-								 // "targetid": d.targetid,
+								 "sourceid": d.sourceid,
+								 "targetid": d.targetid,
 								 "value": +d.count,
 								 "info": d.PII});
 			 });
 
 			 // return only the distinct / unique nodes
-			 graph.nodes = d3.keys(d3.nest()
+			 graph.nodes = d3.entries(d3.nest()
 			   .key(function (d) { return d.name; })
-			   // .key(function (d) { return {"name": d.name, "id": d.id};)
 			   .map(graph.nodes));
-
+			 
+			 //for graph.link to find the link
+			 graph.nodes.forEach(function (d,i){
+			  nodes_k.push(d.key) ;
+			  });
+			 
 			 // loop through each link replacing the text with its index from node
 			 graph.links.forEach(function (d, i) {
-			   graph.links[i].source = graph.nodes.indexOf(graph.links[i].source);
-			   graph.links[i].target = graph.nodes.indexOf(graph.links[i].target);
+			   graph.links[i].source = nodes_k.indexOf(graph.links[i].source);
+			   graph.links[i].target = nodes_k.indexOf(graph.links[i].target);
 			 });
 
 			 //now loop through each nodes to make nodes an array of objects
 			 // rather than an array of strings
 			 graph.nodes.forEach(function (d, i) {
-			   graph.nodes[i] = { "name": d };
-			   // graph.nodes[i] = { "name": d.name, "id": d.id  };
+			   // graph.nodes[i] = { "name": d };
+			   graph.nodes[i] = { "name": d.key, "id": d.value[0].id  };
 			 });
 
 		  sankey
@@ -150,16 +158,16 @@ angular.module('interfacesv2', ['ngRoute'])
 		// add in the nodes
 		  var node = svg.append("g").selectAll(".node")
 			  .data(graph.nodes)
-			.enter().append("g")
+			  .enter().append("g")
 			  .attr("class", "node")
 			  .attr("transform", function(d) { 
 				  return "translate(" + d.x + "," + d.y + ")"; })
-			.call(d3.behavior.drag()
+			  .on("click", mouseclick)
+			  .call(d3.behavior.drag()
 			  .origin(function(d) { return d; })
-			  .on("dragstart", function() { 
-				  this.parentNode.appendChild(this); })
-			  .on("drag", dragmove))
-			  // .on("click", mouseclick);
+			//  .on("dragstart", function() { 
+			//	  this.parentNode.appendChild(this); })
+			  .on("drag", dragmove));
 
 		// add the rectangles for the nodes
 		  node.append("rect")
@@ -167,6 +175,7 @@ angular.module('interfacesv2', ['ngRoute'])
 			  .attr("width", sankey.nodeWidth())
 			  .style("fill", "grey")
 			  .style("stroke", "black")
+			  
 			  // .style("fill", function(d) { 
 				  // return d.color = color(d.name.replace(/ .*/, "")); })
 			  // .style("stroke", function(d) { 
@@ -201,6 +210,18 @@ angular.module('interfacesv2', ['ngRoute'])
 			sankey.relayout();
 			link.attr("d", path);
 		  }
+		  
+		  function mouseclick(d) {
+			var appid = d.id;
+            window.location = window.location.pathname + ('#!/applications/' + appid);
+     		//	$scope.$apply();
+
+            }
+
+		// function nozoom() {
+		  // d3.event.preventDefault();
+		// }			
+			
         // I need to learn javascript
   var numCycles = 0;
   for( var i = 0; i< sankey.links().length; i++ ) {
@@ -208,18 +229,18 @@ angular.module('interfacesv2', ['ngRoute'])
       numCycles++;
     }
   }
- var cycleTopMarginSize = -10;
+ var cycleTopMarginSize = -10;/* 
  var horizontalMarginSize = 5;
-  /* var cycleTopMarginSize = (sankey.cycleLaneDistFromFwdPaths() -
-	    ( (sankey.cycleLaneNarrowWidth() + sankey.cycleSmallWidthBuffer() ) * numCycles ) )
-  var horizontalMarginSize = ( sankey.cycleDistFromNode() + sankey.cycleControlPointDist() ); */
+  // var cycleTopMarginSize = (sankey.cycleLaneDistFromFwdPaths() -
+	    // ( (sankey.cycleLaneNarrowWidth() + sankey.cycleSmallWidthBuffer() ) * numCycles ) )
+  // var horizontalMarginSize = ( sankey.cycleDistFromNode() + sankey.cycleControlPointDist() ); 
 
   svg = d3.select('#' + CONTAINER_ID).select("svg")
     .attr( "viewBox",
 	  "" + (0 - horizontalMarginSize ) + " "         // left
 	  + cycleTopMarginSize + " "                     // top
 	  + (w + horizontalMarginSize * 2 ) + " "     // width
-	  + (h + (-1 * cycleTopMarginSize)) + " " );  // height
+	  + (h + (-1 * cycleTopMarginSize)) + " " );  // height */
       
 	   //Insert Legend			
 		    var legend = svg.selectAll(".legend")
@@ -241,17 +262,12 @@ angular.module('interfacesv2', ['ngRoute'])
 			  .attr("x", w - 5)
 			  .attr("y", 8)
 			  .attr("dy", ".35em")
-        .style("font-size", "12px")
-        .style("font-weight", "bold")
+			  .style("font-size", "12px")
+			  .style("font-weight", "bold")
 			  .style("text-anchor", "end")
 			  .text(function(d) { return d; });  
 
-        // function mouseclick(d) {
-            // var appid = d.id;
-            // $location.path('/applications/' + appid);
-            // $scope.$apply();
 
-            // }
 			
       
 		});
