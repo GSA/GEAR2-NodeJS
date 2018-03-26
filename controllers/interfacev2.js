8cf869cb07ecf046793af312709ca57dcae6c9ad
aliang-gsa <amy.liang@gsa.gov>
2018-03-26 17:10:07 -0400

    testing wip

--- controllers/interfacev2.js
+++ controllers/interfacev2.js
@@ -4,11 +4,16 @@ const interfacev2Store = new Interfacev2Store();
 
 function findAll(req, res) {
 
-  interfacev2Store.query(`call get_application_interfacesv3( ${req.params.id} )`, (results) => {
+  interfacev2Store.search(`call get_application_interfacesv3(0)`, (results) => {
     res.json(results);
   });
 }
-
+function findOne(req, res) {
+  interfacev2Store.search(`call get_application_interfacesv3( ${req.params.id}`, (results) => {
+    res.json(results);
+  });
+}
 module.exports = {
   findAll,
+  findOne,
 };
