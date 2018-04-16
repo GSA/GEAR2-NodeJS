02f0d88128b2f911d048b7aff25ac363a0b5a33f
dodsonm <matthew.dodson.dc@gmail.com>
2018-04-16 10:55:26 -0400

    Add DELETE functionality to edit form

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { jsonServerRestClient, Admin, Resource } from 'admin-on-rest';
+import { jsonServerRestClient, Admin, Resource, Delete } from 'admin-on-rest';
 import epilogueClient from 'aor-epilogue-client';
 import { FismaList, FismaEdit, FismaCreate } from './Fisma';
 import { ArtifactsList, ArtifactsEdit, ArtifactsCreate } from './Artifacts';
 import { PocsList, PocsEdit, PocsCreate } from './Pocs';
 
 const App = () => (
-    <Admin restClient={epilogueClient('http://127.0.0.1:3333')}>
-      <Resource name="fisma" list={FismaList} edit={FismaEdit} create={FismaCreate} />
-      <Resource name="artifacts" list={ArtifactsList} edit={ArtifactsEdit} create={ArtifactsCreate} />
-      <Resource name="pocs" list={PocsList} edit={PocsEdit} create={PocsCreate} />
+    <Admin restClient={epilogueClient('http://localhost:3333')}>
+      <Resource name="fisma" list={FismaList} edit={FismaEdit} create={FismaCreate} remove={Delete} />
+      <Resource name="artifacts" list={ArtifactsList} edit={ArtifactsEdit} create={ArtifactsCreate} remove={Delete} />
+      <Resource name="pocs" list={PocsList} edit={PocsEdit} create={PocsCreate} remove={Delete} />
     </Admin>
 );
 
