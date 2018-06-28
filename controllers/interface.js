bbe30c8048a243911d05b50de26c9a08b7a0e19a
Xiaomeng Liang <1658398656@gsa.gov>
2018-06-28 10:20:16 -0400

    update interfaces controllers

--- controllers/interface.js
+++ controllers/interface.js
@@ -5,10 +5,10 @@ const interfaceStore = new InterfaceStore();
 function findAll(req, res) {
   let filter = '';
   if (req.query.owner) {
-    filter = ` \'WHERE owner1.Keyname LIKE \\\'%${req.query.owner}%\\\' or owner2.Keyname LIKE \\\'%${req.query.owner}%\\\' ORDER BY owner1.Keyname, owner2.Keyname\'`;
+    filter = ` \'WHERE ( owner1.Keyname LIKE \\\'%${req.query.owner}%\\\' or owner2.Keyname LIKE \\\'%${req.query.owner}%\\\') AND appstat1.Keyname <> \\\'Retired\\\'  AND appstat2.Keyname <> \\\'Retired\\\'  ORDER BY owner1.Keyname, owner2.Keyname \'`;
   }
   if (req.query.sys) {
-    filter = ` \'WHERE sys1.Keyname LIKE \\\'%${req.query.sys}%\\\' or sys2.Keyname LIKE \\\'%${req.query.sys}%\\\' ORDER BY sys1.Keyname, sys2.Keyname\'`;
+    filter = ` \'WHERE ( sys1.Keyname LIKE \\\'%${req.query.sys}%\\\' or sys2.Keyname LIKE \\\'%${req.query.sys}%\\\') AND appstat1.Keyname <> \\\'Retired\\\'  AND appstat2.Keyname <> \\\'Retired\\\'  ORDER BY sys1.Keyname, sys2.Keyname \'`;
   }
   interfaceStore.search(`call get_application_interfaces( ${filter} )`, (results) => {
     res.set({
