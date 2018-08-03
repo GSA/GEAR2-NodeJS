21cbeb23235242c64e8598fbf48b1ec27cca8118
dodsonm <matthew.dodson.dc@gmail.com>
2018-08-03 13:23:49 -0400

    WIP Passport SAML + JWT deconstructing

---
+++ passport.js
@@ -0,0 +1,18 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password'
+    },
+    function (email, password, cb) {
+        //this one is typically a DB call. Assume that the returned user object is pre-formatted and ready for storing in JWT
+        return UserModel.findOne({email, password})
+           .then(user => {
+               if (!user) {
+                   return cb(null, false, {message: 'Incorrect email or password.'});
+               }
+               return cb(null, user, {message: 'Logged In Successfully'});
+          })
+          .catch(err => cb(err));
+    }
+));
--- server.js
+++ server.js
@@ -9,7 +9,9 @@ var
     bodyParser = require('body-parser'),
     cors = require('cors'),
     passport = require('passport'),
-    Strategy = require('passport-saml').Strategy,
+    SAMLStrategy = require('passport-saml').Strategy,
+    JwtStrategy = require('passport-jwt').Strategy,
+    ExtractJwt = require('passport-jwt').ExtractJwt,
     session = require('express-session'),
     mysql = require('mysql2'),
     util = require('util'),
@@ -33,25 +35,28 @@ const samlConfig = {
   entryPoint: process.env.SAML_ENTRY_POINT,
   issuer: process.env.SAML_ISSUER,
 };
-// user co/dec a required passport thing
+// these 2 serialize & deserialize user functions are a passport pattern
 passport.serializeUser(function (user, done) {
   done(null, user);
 });
 passport.deserializeUser(function (user, done) {
   done(null, user);
 });
-passport.use(new Strategy(samlConfig, (secureAuthProfile, cb) => {
+// PASSPORT SAML STRATEGY
+passport.use(new SAMLStrategy(samlConfig, (secureAuthProfile, cb) => {
   const dbConn = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database:'acl',
   });
+  //TODO: IS THIS SQL UP TO DATE?? 8/3/18
   const sqlString = `CALL acl.get_user_perms('${secureAuthProfile.nameID}');`;
   dbConn.connect();
   dbConn.query(sqlString, (err, results, fields) => {
     if (err) {
       secureAuthProfile.dbError = util.inspect(err);
+      // PURPOSELY AVOIDING A LOG-IN ERROR & SENDING DB ERROR ALONG WITH PROFILE.
       return cb(null, secureAuthProfile);
     } else {
       secureAuthProfile.dbError = null;
@@ -60,8 +65,30 @@ passport.use(new Strategy(samlConfig, (secureAuthProfile, cb) => {
     }
   });
 }));
-/********************************************************************/
+// PASSPORT JWT STRATEGY
+// TODO: do NOT got to production without securing signing key
+passport.use(new JwtStrategy({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey   : 'your_jwt_secret'
+    },
+    function (jwtPayload, cb) {
+      //find the user in db if needed
+      console.log('\n\nFFFAAAARRRBBL\n\n');
+      return cb(null, {
+        id:1,
+        un:'mfagjhzf',
+      })
+      // return UserModel.findOneById(jwtPayload.id)
+      //     .then(user => {
+      //         return cb(null, user);
+      //     })
+      //     .catch(err => {
+      //         return cb(err);
+      //     });
+    }
+));
 
+/********************************************************************/
 // LOAD MIDDLEWARE
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -69,7 +96,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 /********************************************************************
-(ADDING FOR PASSPORT) MIDDLEWARE
+PASSPORT & SESSION MIDDLEWARE (Introduced with Passport-SAML)
 ********************************************************************/
 app.use(session(
   {
@@ -80,17 +107,19 @@ app.use(session(
 app.use(passport.initialize());
 app.use(passport.session());
 
-/**** TEMPORARYILY DISABLED!! ****/
-/* to restore, uncomment 'if-else' block and delete preceding 'res.sendFile' line */
-/******************************************************************/
+/********************************************************************
+ADMIN GATEWAY
+********************************************************************/
 app.get('/admin', function (req, res) {
-  // res.sendFile(path.join(__dirname, 'public', 'admin', 'index.html'));
   if (req.isAuthenticated()) {
     res.sendFile(path.join(__dirname, 'public', 'admin', 'index.html'));
   } else {
     res.redirect('/login')
   }
 });
+/********************************************************************
+PUBLIC STATIC
+********************************************************************/
 app.use(express.static(path.join(__dirname, 'public')));
 
 /********************************************************************
@@ -101,24 +130,44 @@ app.get('/pass', function (req, res) {
   if (req.isAuthenticated()) {
     res.redirect('/admin/#/');
   } else {
-    res.send('NO PASS.');
+    res.status = 403
+    res.send('UNAUTHORIZED');
   }
 });
+/********************************************************************
+LOG IN
+********************************************************************/
+app.get('/pp', passport.authenticate('jwt', { session: false }),
+    function(req, res) {
+        res.send(req.user.profile);
+    }
+);
 app.get('/login',
   passport.authenticate('saml', {
       successRedirect: '/pass',
       failureRedirect: '/login'
     })
   );
+/********************************************************************
+SAML CALLBACK ROUTE. This will receive SAML assertion response
+********************************************************************/
 app.post(samlConfig.path,
-  passport.authenticate('saml', {successRedirect: '/pass', failureRedirect:'/pass'}),
-  (req, res) => {res.redirect('/pass')}
+  passport.authenticate('saml',{ successRedirect: '/pass', failureRedirect:'/pass' }),
+  (req, res) => {
+    res.redirect('/pass')
+  }
 );
+/********************************************************************
+LOG OUT
+********************************************************************/
 app.get('/logout', function (req, res) {
   req.logout();
   // TODO: invalidate session on IP
   res.redirect('/pass');
 });
+/********************************************************************
+GET USER STATUS
+********************************************************************/
 app.get('/ustat', (req, res) => {
   if (req.user) {
     res.json({
@@ -134,7 +183,8 @@ app.get('/ustat', (req, res) => {
     });
   }
 });
-/***************************************************/
+/*******************************************************************/
+/*******************************************************************/
 
 
 var server = http.createServer(app);
