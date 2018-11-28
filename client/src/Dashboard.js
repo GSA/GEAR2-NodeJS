0c8ca0a0503e5b9406f9239f562b9e15f9723181
VigneshRangaraj <4324532512@gsa.gov>
2018-11-28 11:36:16 -0500

    Remove in-line styling from Dash-board.js

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,55 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
-import decodeJwt from 'jwt-decode';
 
 // const decodedJwt = decodeJwt(localStorage.jwt);
 const content = {
   tstamp: `10 AUG 2018 MORNING`,
-  pageTitle: `GEAR 2.0 Pre-Release Admin Portal`,
-  // user: decodedJwt.un,
+  pageTitle: `GEAR 2.0 Pre-Release Admin Portal`
 }
-
-const styles = {
-  card: {
-    minWidth: 275,
-  },
-  title: {
-    fontSize: 14,
-  },
-  warn: {
-    marginTop: 1,
-    lineHeight: 1.25,
-  }
-};
 
 function SimpleCard(props) {
   const { classes } = props;
 
   return (
     <div>
-      <Card className={classes.card}>
-        <CardContent>
-          <Typography className={classes.title} color="textSecondary">
-          </Typography>
-          <Typography variant="headline" component="h2">
-            {content.pageTitle}
-          </Typography>
-        </CardContent>
-      </Card>
-      <Card className={classes.card}>
-        <CardContent>
-          <Typography className={classes.title} color="textSecondary" align="center">
-            Warning: Official Use Only
-          </Typography>
-          <Typography className={classes.warn} variant="body2" align="center">
-            ***WARNING*** This is a U.S. General Services Administration Federal Government computer system that is "FOR OFFICIAL USE ONLY." This system is subject to monitoring. Individuals found performing unauthorized activities are subject to disciplinary action including criminal prosecution.
-          </Typography>
-        </CardContent>
-      </Card>
+      <br/>
+      <br/>
+      {content.pageTitle}
+      <br/>
+      <br/>
+      Warning: Official Use Only
+      <br/>
+      ***WARNING*** This is a U.S. General Services Administration Federal Government computer system that is "FOR OFFICIAL USE ONLY." This system is subject to monitoring. Individuals found performing unauthorized activities are subject to disciplinary action including criminal prosecution.
     </div>
   );
 }
@@ -58,4 +28,4 @@ SimpleCard.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(SimpleCard);
+export default SimpleCard;
