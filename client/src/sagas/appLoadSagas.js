12cc82ee746360406cb69ec5e316d062813c6ca1
Clara Bridges <clarapbridges@gmail.com>
2018-12-10 14:49:58 -0500

    listening for a CRUD UPDATE action and deciding which resources to reload

--- client/src/sagas/appLoadSagas.js
+++ client/src/sagas/appLoadSagas.js
@@ -1,4 +1,4 @@
-import { call, put, takeLatest } from 'redux-saga/effects';
+import { select, put, take, takeLatest } from 'redux-saga/effects';
 import * as types from '../actions/actionTypes';
 import * as capabilitiesActions from "../actions/capabilitiesActions";
 
@@ -20,6 +20,19 @@ function* fetchBackground(dispatch) {
     }
 }
 
+function* whatUp(state) {
+    switch(state.meta.resource) {
+        case 'pocs':
+            console.log('getting pocs');
+            yield put({type: types.LOAD_POCS});
+            break;
+        default:
+            break;
+    }
+}
+
 export default function* watchEVERYTHING() {
     yield takeLatest('RA/SET_SIDEBAR_VISIBILITY', fetchBackground);
+    yield takeLatest('RA/CRUD_UPDATE_SUCCESS', whatUp);
 }
+
