949fc6bd2c473fb90091b5ddab73578f4a0fee54
vigneshrangaraj <rangaraj.vignesh@gmail.com>
2019-01-10 17:39:25 -0500

    Accidental deletes

--- api/v1/models/obj_fisma.js
+++ api/v1/models/obj_fisma.js
@@ -7,13 +7,13 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
-      field: 'GEAR_ID'
+      field: 'ex:GEAR_ID'
     },
     keyname: {
       type: DataTypes.STRING,
       allowNull: false,
-      field: 'System_Name'
-    }
+      field: 'ex:System_Name'
+    },
   }, {
     name: {
       singular: 'fisma',
@@ -22,6 +22,37 @@ module.exports = function(sequelize, DataTypes) {
     timestamps: false,
     tableName: 'obj_fisma_archer',
   });
+
+  fisma.associate = function (models) {
+    models.fisma.belongsToMany(models.fismaArtifact, {
+      as: 'fisma_artifacts',
+      foreignKey: 'objFismaSystem_Id',
+      otherKey: 'objFISMAartifact_Id',
+      through: 'zk_fisma_artifact',
+      timestamps: false,
+    });
+    models.fisma.belongsToMany(models.poc, {
+      as: 'issm',
+      foreignKey: 'objFismaSystem_Id',
+      otherKey: 'objPOC_Id',
+      through: 'zk_fisma_issm',
+      timestamps: false,
+    });
+    models.fisma.belongsToMany(models.poc, {
+      as: 'isso',
+      foreignKey: 'objFismaSystem_Id',
+      otherKey: 'objPOC_Id',
+      through: 'zk_fisma_isso',
+      timestamps: false,
+    });
+    models.fisma.belongsToMany(models.fismareplacedby, {
+      as: 'replaced_by',
+      foreignKey: 'objFismaSystem_Id_Old',
+      otherKey: 'objFismaSystem_Id_New',
+      through: 'zk_fisma_replacedby',
+      timestamps: false,
+    });
+  }
 
   return fisma;
 };
