f594072e9cc172ced33bb422c3a9e3f2a637bd9b
vigneshrangaraj <rangaraj.vignesh@gmail.com>
2019-02-22 11:16:21 -0500

    Delete GEAR folder

--- gear/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import courses from './courseReducer';
-
-const rootReducer = combineReducers({
-    //this is the prop name in state referenced in mapStateToProps
-    courses: courses
-});
-
-export default rootReducer;
